7287a32a2b53eb91011c7c3b82fd86f407a5961b
Masateru, mmiy, Miyasaka <miyasaka@gmail.com>
2025-12-05 09:13:25 +0900

    clean up

--- kernel/target/x86_64-unknown-none/release/deps/bit_field-bd320d7b0e16bbbf.d
+++
@@ -1,7 +0,0 @@
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/bit_field-bd320d7b0e16bbbf.d: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bit_field-0.10.3/src/lib.rs
-
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/libbit_field-bd320d7b0e16bbbf.rlib: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bit_field-0.10.3/src/lib.rs
-
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/libbit_field-bd320d7b0e16bbbf.rmeta: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bit_field-0.10.3/src/lib.rs
-
-/home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bit_field-0.10.3/src/lib.rs:
--- kernel/target/x86_64-unknown-none/release/deps/linked_list_allocator-81e65aedc3452865.d
+++
@@ -1,8 +0,0 @@
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/linked_list_allocator-81e65aedc3452865.d: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/lib.rs /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/hole.rs
-
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/liblinked_list_allocator-81e65aedc3452865.rlib: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/lib.rs /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/hole.rs
-
-/home/mmiy/workspace/retron/kernel/target/x86_64-unknown-none/release/deps/liblinked_list_allocator-81e65aedc3452865.rmeta: /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/lib.rs /home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/hole.rs
-
-/home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/lib.rs:
-/home/mmiy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/linked_list_allocator-0.10.5/src/hole.rs:
